4f31b40dfc4e80c7dbc4857722e4a7d5c0868ce0
Nando <mnandohidayat@gmail.com>
2018-08-23 20:22:47 +0700

    add 00 Setup with simple node server

---
+++ 00 Setup/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const json = require('./data.json');
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.json(json);
+});
+
+app.listen(3000);
