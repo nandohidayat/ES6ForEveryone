81be6e406a493be1324a3970575e212f4404b8a2
Nando <mnandohidayat@gmail.com>
2018-08-25 17:02:02 +0700

    make my own module in 'src' and import it inside app.js

--- 13 JavaScript Modules and Using npm/es6modules/app.js
+++ 13 JavaScript Modules and Using npm/es6modules/app.js
@@ -1,6 +1,11 @@
 import { uniq } from 'lodash';
 import insane from 'insane';
 import jsonp from 'jsonp';
+import { apiKey as key, url, sayHi, old, dog } from './src/config';
+
+console.log(Key, url);
+
+sayHi('Nando');
 
 const ages = [27, 6, 97, 26, 1, 98];
 console.log(uniq(ages));
